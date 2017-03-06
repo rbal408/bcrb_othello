a0ba7a0f2bd14402dcd0b0fd8c241d5afc5451e2
Brent Cahill <brent.cahill@yahoo.com>
2017-03-05 18:40:08 -0600

    Added a two-node linked list class to be used for Heuristic valueation

--- player.cpp
+++ player.cpp
@@ -9,6 +9,12 @@ Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
 
+    Board this_board = new Board();
+
+    Side our_side = side;
+
+
+
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
@@ -44,5 +50,56 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
+
+    // A copy board from which to check move values for the Heuristic values.
+    Board board_copy = this_board.copy();
+
+
+    /* Theoretically, instead of having two vectors which had Moves and scores
+     * respecitively, we could create a linked list which had three values,
+     * the move, the value and a pointer to the next. This would liekly be
+     * much more efficient. I have implemented this type of list in the 
+     * player.hpp file.
+     */
+    std::vector<Move> moves; // Vector of possible moves
+    std::vector<int> scores; // Vector of scores for each move
+    Move our_move;           // The move we decide to make
+
+    /* This checks for all possible moves and adds them to a vector of moves.
+     * We can then check this vector to see which move we want to actually
+     * make.
+     */
+    while(this_board.hasMoves(our_side)){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Move curr = (i, j);
+                if (checkMove(curr, our_side) == true)
+                {
+                    moves.push_back(curr);
+                }
+            }
+        }
+
+        /* This assignes a Heuristic score to each possible move by our_side.
+         * We will iterate through all moves to determine the scores for each.
+         */
+        for (int i = 0; i < moves.size(); i++){
+            /* TODO
+             * Ideas:
+             * Assign a _score_ based on the number of stones captured by the
+             * move, then:
+             *
+             * If the square is a side: (score * 3)
+             * If the square is a corner: (score * 4)?
+             * If the square is one before a side/corner: (score * (-3))
+             */
+
+            board_copy.doMove()
+        }
+
+        this_board.doMove(our_move, our_side);
+        moves.clear();
+    }
+
     return nullptr;
 }
--- player.hpp
+++ player.hpp
@@ -2,6 +2,7 @@
 #define __PLAYER_H__
 
 #include <iostream>
+#include <tuple>
 #include "common.hpp"
 #include "board.hpp"
 using namespace std;
@@ -17,5 +18,40 @@ public:
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
 };
+
+class two_node_ll {
+
+    struct Node {
+        Move move;
+        int scr;
+        Node *next;
+    };
+
+public:
+
+    two_node_ll(){
+        head = NULL;
+    }
+
+    void push(Move mv, int scr)
+    {
+        Node *n = new Node();
+        n->move = mv;
+        n->score = scr;
+        head = n;
+    }
+
+    std::tuple<Move, int> pop();
+    {
+        Node *n = head;
+        Move mv = n->move;
+        int scr = n->score;
+
+        head = head->next;
+        delete n;
+        std::tuple<Move, int> val = make_tuple(mv, scr);
+        return val;
+    }
+}
 
 #endif
